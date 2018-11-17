5de0e2ca7cfd2cbdc42a3f8daa9e3fcd470917a9
Milton Daniel <miltondaniel29.mdf@gmail.com>
2018-11-17 10:39:33 -0600

    new heroku cleardb configuration

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,7 +4,7 @@ class Conexion {
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=pos","root","");
+		$link = new PDO("mysql:host=us-cdbr-iron-east-01.cleardb.net;dbname=pos","bf50f917fb27b6","1915e23f");
 
 		$link -> exec("set names utf8");
 
