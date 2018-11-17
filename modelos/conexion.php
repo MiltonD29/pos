4375b26ace0f8f2ce996c25a24b59e0ae11681f2
Milton Daniel <miltondaniel29.mdf@gmail.com>
2018-11-17 12:44:35 -0600

    fix: arreglar archivo database.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -1,12 +1,10 @@
 <?php
 
-/*mysql://bf50f917fb27b6:1915e23f@us-cdbr-iron-east-01.cleardb.net/heroku_aaf7f9df83d10c5?reconnect=true*/
-
 class Conexion {
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=us-cdbr-iron-east-01.cleardb.net:3306;dbname=pos","bf50f917fb27b6","1915e23f");
+		$link = new PDO("mysql:host=localhost;dbname=pos","root","");
 
 		$link -> exec("set names utf8");
 
