86260071fad4c3275ed3bd3645788f0824613b32
Milton Daniel <miltondaniel29.mdf@gmail.com>
2018-11-17 10:46:21 -0600

    chore: cambiar archivo de conexion

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,7 +4,7 @@ class Conexion {
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=us-cdbr-iron-east-01.cleardb.net;dbname=pos","bf50f917fb27b6","1915e23f");
+		$link = new PDO("mysql:host=localhost;dbname=pos","root","");
 
 		$link -> exec("set names utf8");
 
